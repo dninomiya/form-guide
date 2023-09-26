34da451cfda9150b23b94b684243f13fcdd94108
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-09-26 23:29:21 +0900

    fix(nextconfig): trailingSlash追加

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/form-guide' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/form-guide' : '',
+  trailingSlash: true,
   experimental: {
     mdxRs: true,
   },
