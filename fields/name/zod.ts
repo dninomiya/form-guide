e849fdaef7ecd7613b472535a1bb104b10a4cab7
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-09-29 20:44:24 +0900

    feat(name-field): 文字形式のバリデーションを除去
    日本語英語以外の文字も考慮

--- fields/name/zod.ts
+++ fields/name/zod.ts
@@ -5,9 +5,6 @@ export default z
   .nonempty({
     message: '名前を入力してください。',
   })
-  .regex(/^[A-Za-zぁ-んァ-ン一-龠々〆ヵヶー\s'-]+$/, {
-    message: '不正な文字が含まれています。',
-  })
   .max(60, {
     message: '最大60文字までです。',
   });
