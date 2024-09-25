de5cd2783958f76c888c9fae00a4047dcae122d8
nino <daichi.ninomiya@deer.co.jp>
2024-09-25 15:22:21 +0900

    Update zod.ts

--- fields/password/zod.ts
+++ fields/password/zod.ts
@@ -8,9 +8,6 @@ export default z
   .min(8, {
     message: '最小8文字以上です。',
   })
-  .max(64, {
-    message: '最大64文字までです。',
-  })
   .refine(isStrongPassword, {
     message: '大文字を含む半角英数字と記号を含めてください。',
   });
